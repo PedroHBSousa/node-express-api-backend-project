0399b149869028dd6d25e1caf16b1c10cec24ac8
Pedro Sousa <pedro795027@gmail.com>
2024-07-31 22:19:02 -0300

    feat: configuração do projeto

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+// const express = module('express')
+import express from 'express'
+
 const app = express()
 const port = 3000
+
+const user = {
+    nome: "pedro", 
+    email:"pedro@gmail.com"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/user', (req, res) => {
+    res.json(user)
+  })
+  
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
